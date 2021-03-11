abe41a07418e244c7b2377d63dd82016f871435b
parhamsamee <hallsamee@gmail.com>
2021-03-11 12:54:39 -0600

    set-up

---
+++ src/main/java/org/launchcode/enumerableplanets/models/Planets.java
@@ -0,0 +1,9 @@
+package org.launchcode.enumerableplanets.models;
+
+public enum Planets {
+    // list the planets here.
+    // Mercury, Venus, Earth, Mars, Jupiter,
+    // Saturn, Uranus, Neptune
+    // Don't forget to capitalization convention and enum
+    // syntax to separate value and end the list
+}
